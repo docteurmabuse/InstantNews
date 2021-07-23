e8821775fc1f3d815272b233c02d2d78e349d45c
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-07-23 18:34:42 +0200

    create POJO for newsApi.org

---
+++ app/src/main/java/com/tizzone/instantnewsapplication/data/network/model/NewsResponse.kt
@@ -0,0 +1,18 @@
+package com.tizzone.instantnewsapplication.data.network.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class NewsResponse(
+
+	@field:SerializedName("totalResults")
+	val totalResults: Int? = null,
+
+	@field:SerializedName("articles")
+	val articles: List<ArticlesItem?>? = null,
+
+	@field:SerializedName("status")
+	val status: String? = null
+) : Parcelable
