5a468a20ea52c54f44ec6a9b55b4d1aca0263362
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-07-23 18:17:44 +0200

    create packages for clean architecture and move MainActivity.kt to presentation package

--- app/src/main/java/com/tizzone/instantnewsapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/tizzone/instantnewsapplication/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tizzone.instantnewsapplication
+package com.tizzone.instantnewsapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tizzone.instantnewsapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
