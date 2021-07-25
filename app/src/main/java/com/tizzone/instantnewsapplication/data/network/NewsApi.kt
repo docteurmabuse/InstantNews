c91aac78eb38f0002a5b9fc73fdb961999920a9c
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-07-25 18:31:28 +0200

    update NewsApi.kt -> invert order of pageSize and page

--- app/src/main/java/com/tizzone/instantnewsapplication/data/network/NewsApi.kt
+++ app/src/main/java/com/tizzone/instantnewsapplication/data/network/NewsApi.kt
@@ -10,10 +10,10 @@ interface NewsApi {
     suspend fun getHeadLines(
         @Query("country")
         countryCode: String = "fr",
-        @Query("page")
-        page:Int =1,
         @Query("pageSize")
         pageSize: Int = 10,
+        @Query("page")
+        page:Int =1,
         @Query("apiKey")
         newsApiKey: String = NEWS_API_KEY
     ): NewsResponse
