c524c0f0ffdaf3959abbf1f1309a6bd33f5bfcd3
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-07-25 15:35:09 +0200

    - create ArticlesListViewModel.kt & DataState.kt

--- app/src/main/java/com/tizzone/instantnewsapplication/presentation/ui/ArticlesListViewModel.kt
+++ app/src/main/java/com/tizzone/instantnewsapplication/presentation/ui/ArticlesListViewModel.kt
@@ -1,8 +1,18 @@
 package com.tizzone.instantnewsapplication.presentation.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.tizzone.instantnewsapplication.domain.data.DataState
+import com.tizzone.instantnewsapplication.domain.model.Article
 import com.tizzone.instantnewsapplication.domain.usecases.articles_list.GetAllHeadlines
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +20,24 @@ class ArticlesListViewModel
 @Inject constructor(
     private val getAllHeadlines: GetAllHeadlines,
 ) : ViewModel() {
+    private val _stateArticles =
+        MutableStateFlow<DataState<PagingData<Article>>>(DataState.loading(null))
+    val stateArticles: StateFlow<DataState<PagingData<Article>>> get() = _stateArticles
 
+    init {
+        fetchAllHeadlines()
+    }
+
+    private fun fetchAllHeadlines() {
+        viewModelScope.launch {
+            getAllHeadlines.invoke()
+                .catch { e ->
+                    _stateArticles.value = DataState.error("Error: ${e.toString()}", null)
+                }
+                .cachedIn(viewModelScope)
+                .collectLatest { pagingDataArticles ->
+                    _stateArticles.value = DataState.success(pagingDataArticles)
+                }
+        }
+    }
 }
