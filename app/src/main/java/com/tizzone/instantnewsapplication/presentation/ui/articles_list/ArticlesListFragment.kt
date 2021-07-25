3a8c038c24c3eedecd8d272b433a363de766d3f6
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-07-25 17:54:52 +0200

    update ArticlesListFragment.kt -> add viewModel observer and fill adapter with list

--- app/src/main/java/com/tizzone/instantnewsapplication/presentation/ui/articles_list/ArticlesListFragment.kt
+++ app/src/main/java/com/tizzone/instantnewsapplication/presentation/ui/articles_list/ArticlesListFragment.kt
@@ -5,16 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.tizzone.instantnewsapplication.databinding.FragmentArticlesListBinding
+import com.tizzone.instantnewsapplication.domain.data.DataState
 import com.tizzone.instantnewsapplication.domain.model.Article
 import com.tizzone.instantnewsapplication.presentation.ui.adapters.ArticlesRecyclerViewAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 /**
  * A fragment representing a list of Items.
@@ -22,7 +28,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ArticlesListFragment : Fragment(), ArticlesRecyclerViewAdapter.Interaction {
 
-    private var columnCount = 1
     private var _binding: FragmentArticlesListBinding? = null
 
     //This property is only available between onCreateView & onDestroyView.
@@ -32,14 +37,11 @@ class ArticlesListFragment : Fragment(), ArticlesRecyclerViewAdapter.Interaction
     private val viewModel: ArticlesListViewModel by viewModels()
 
     //Views
-    lateinit var swipeRefreshLayout: SwipeRefreshLayout
-    lateinit var progressBar: ProgressBar
-    lateinit var articlesAdapter: ArticlesRecyclerViewAdapter
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var progressBar: ProgressBar
+    private lateinit var articlesAdapter: ArticlesRecyclerViewAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var newJobs: Job? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,6 +54,34 @@ class ArticlesListFragment : Fragment(), ArticlesRecyclerViewAdapter.Interaction
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        setObserver()
+    }
+
+    private fun setObserver() {
+        newJobs?.cancel()
+        newJobs = lifecycleScope.launch {
+            val value = viewModel.stateArticles
+            value.collectLatest {
+                when (it.status) {
+                    DataState.Status.SUCCESS -> {
+                        it.data?.let { articles ->
+                            articlesAdapter.submitData(articles)
+                        }
+                    }
+                    DataState.Status.ERROR -> {
+                        displayError(it.message)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun displayError(message: String?) {
+        if (message != null) {
+            Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(requireContext(), "Unknown error", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun initViews() {
