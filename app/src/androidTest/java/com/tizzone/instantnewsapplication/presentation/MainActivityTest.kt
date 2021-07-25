c0f7b8ff24a96c1ac44c134e3a7713c9219c9f47
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-07-25 22:42:29 +0200

    update ArticleDetailFragment.kt -> add onDestroy method override

--- app/src/androidTest/java/com/tizzone/instantnewsapplication/presentation/MainActivityTest.kt
+++ app/src/androidTest/java/com/tizzone/instantnewsapplication/presentation/MainActivityTest.kt
@@ -1,9 +1,13 @@
 package com.tizzone.instantnewsapplication.presentation
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
 }
